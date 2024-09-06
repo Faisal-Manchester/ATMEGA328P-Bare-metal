df85df738a4e91b923f0ba497aaa9fa6d4d8c839
Faisal-Manchester <106784062+Faisal-Manchester@users.noreply.github.com>
2024-09-06 15:28:55 +0100

    Update sketch_feb27b_question1_attempt1.ino
    Indenting and commenting of old code was done

--- sketch_feb27b_question1_attempt1.ino
+++ sketch_feb27b_question1_attempt1.ino
@@ -1,96 +1,59 @@
-class Blinky
+#define BIT(x) (1 << (x))
 
+class Blinky
 {
- private:
-  unsigned char *port;
-  unsigned int pin;
-  int OFF;
-  int ON;
-  unsigned char *port_Dir;
-  
+  private:
+    unsigned char *port;
+    unsigned int pin;
+    int OFF;
+    int ON;
+    unsigned char *port_Dir;
   
   public:
-    Blinky(unsigned char* port_number,unsigned int pin_Number, int ON_time,int OFF_time)
-    
-    { 
-     //#define BIT(pin_Number) (1 << (pin_Number))
-     ON=ON_time;
-     OFF=OFF_time;
-     port=port_number;
-     pin=pin_Number;
-    // pin=pin_Number;
-   if(port==(unsigned char*)0x25)
-     {
-     port_Dir =(unsigned char*)0x24;
-     }
-     else if(port ==(unsigned char*)0x28)
-     {
-       port_Dir=(unsigned char*)0x27;
-     }
-     else 
-     {
-      port_Dir=(unsigned char*)0x2A;
-     }
-    // pin=1<<pin;
-    // port_Dir =(unsigned char*)0x24;
-    
-    //*port_Dir=(*port_Dir)&BIT(pin_Number);
-     *port_Dir=0xFF;
-    
+    Blinky(unsigned char* port_number, unsigned int pin_Number, int ON_time, int OFF_time)
+    {
+      ON = ON_time;
+      OFF = OFF_time;
+      port = port_number;
+      pin = pin_Number;
+
+      if(port == (unsigned char*)0x25)
+      {
+        port_Dir = (unsigned char*)0x24;  // DDR for port 0x25
+      }
+      else if(port == (unsigned char*)0x28)
+      {
+        port_Dir = (unsigned char*)0x27;  // DDR for port 0x28
+      }
+      else
+      {
+        port_Dir = (unsigned char*)0x2A;  // Default DDR
+      }
+
+      // Set the specific pin as output
+      *port_Dir |= BIT(pin);
     }
   
- void Blink()
-
-
-{
- *port=(*port)|(1<<pin);
- //*port=0xFF;
-  delay(ON);
- *port=(*port)&~(1<<pin);
- //*port=0x00;
-delay(OFF);
-
-
-}
-
-
-
-
-
-
-
-
-
-
-  
-  
-  
-
-
-
-
-
-
-
-
-
-
+    void Blink()
+    {
+      *port |= BIT(pin);    // Set pin HIGH
+      delay(ON);            // Wait for ON duration
+      *port &= ~BIT(pin);   // Set pin LOW
+      delay(OFF);           // Wait for OFF duration
+    }
 };
-////// Example 7
-Blinky led1((unsigned char*)0x25,0,750,350);
-Blinky led2((unsigned char*)0x25,1,400,600);
 
+// Example usage
+Blinky led1((unsigned char*)0x25, 0, 750, 350);
+Blinky led2((unsigned char*)0x25, 1, 400, 600);
 
 void setup() 
 {
-  
-
+  // Any setup code if needed
 }
 
 void loop() 
 {
   led1.Blink();
   led2.Blink();
-  
-
 }
