d86585865fc02c206688226b645817db2af52965
Faisal-Manchester <106784062+Faisal-Manchester@users.noreply.github.com>
2024-09-06 15:32:07 +0100

    Update sketch_feb27b_question1_example8_attempt1.ino
    Code is indented and appropriately commented

--- sketch_feb27b_question1_example8_attempt1.ino
+++ sketch_feb27b_question1_example8_attempt1.ino
@@ -1,96 +1,57 @@
-class Button
-
-{
- private:
-  unsigned char *port;
-  unsigned int pin;
-  int OFF;
-  int ON;
-  unsigned char *port_Dir;
-  
+// Class to handle a blinking LED connected to a specific pin of a port
+class Blinky {
+  private:
+    unsigned char *port;      // Pointer to the port (address)
+    unsigned int pin;         // Pin number on the port
+    int OFF;                  // OFF time duration (ms)
+    int ON;                   // ON time duration (ms)
+    unsigned char *port_Dir;  // Pointer to the port direction register
   
   public:
-    Blinky(unsigned char* port_number,unsigned int pin_Number, int ON_time,int OFF_time)
-    
-    { 
-     //#define BIT(pin_Number) (1 << (pin_Number))
-     ON=ON_time;
-     OFF=OFF_time;
-     port=port_number;
-     pin=pin_Number;
-    // pin=pin_Number;
-   if(port==(unsigned char*)0x25)
-     {
-     port_Dir =(unsigned char*)0x24;
-     }
-     else if(port ==(unsigned char*)0x28)
-     {
-       port_Dir=(unsigned char*)0x27;
-     }
-     else 
-     {
-      port_Dir=(unsigned char*)0x2A;
-     }
-    // pin=1<<pin;
-    // port_Dir =(unsigned char*)0x24;
-    
-    //*port_Dir=(*port_Dir)&BIT(pin_Number);
-     *port_Dir=0xFF;
-    
+    // Constructor: Initializes the port, pin, and ON/OFF times
+    Blinky(unsigned char* port_number, unsigned int pin_Number, int ON_time, int OFF_time) {
+      ON = ON_time;          // Set the ON time
+      OFF = OFF_time;        // Set the OFF time
+      port = port_number;    // Set the port address
+      pin = pin_Number;      // Set the pin number
+
+      // Set the direction register (DDR) based on the port address
+      if (port == (unsigned char*)0x25) {
+        port_Dir = (unsigned char*)0x24;  // DDR for port 0x25
+      } else if (port == (unsigned char*)0x28) {
+        port_Dir = (unsigned char*)0x27;  // DDR for port 0x28
+      } else {
+        port_Dir = (unsigned char*)0x2A;  // Default DDR for other ports
+      }
+
+      // Set the entire port direction to output (0xFF)
+      // You may want to only set the direction for the specific pin
+      *port_Dir = 0xFF;
     }
-  
- void Blink()
 
+    // Blink function: Turns the LED on and off with the specified ON/OFF times
+    void Blink() {
+      // Set the pin HIGH (turn on the LED)
+      *port = (*port) | (1 << pin);
+      delay(ON);  // Wait for the ON duration
 
-{
- *port=(*port)|(1<<pin);
- //*port=0xFF;
-  delay(ON);
- *port=(*port)&~(1<<pin);
- //*port=0x00;
-delay(OFF);
-
-
-}
-
-
-
-
-
-
-
-
-
-
-  
-  
-  
-
-
-
-
-
-
-
-
-
-
+      // Set the pin LOW (turn off the LED)
+      *port = (*port) & ~(1 << pin);
+      delay(OFF); // Wait for the OFF duration
+    }
 };
-////// Example 7
-Blinky led1((unsigned char*)0x25,0,750,350);
-Blinky led2((unsigned char*)0x25,1,400,600);
 
+// Create two Blinky objects representing two LEDs connected to the same port but different pins
+Blinky led1((unsigned char*)0x25, 0, 750, 350);  // LED on pin 0 of port 0x25
+Blinky led2((unsigned char*)0x25, 1, 400, 600);  // LED on pin 1 of port 0x25
 
-void setup() 
-{
-  
-
+// Setup function (currently empty, but can be used for initialization)
+void setup() {
+  // No setup needed in this case
 }
 
-void loop() 
-{
-  led1.Blink();
-  led2.Blink();
-  
-
+// Main loop: Continuously blink both LEDs
+void loop() {
+  led1.Blink();  // Blink LED1
+  led2.Blink();  // Blink LED2
 }
